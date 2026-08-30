fix: fix rotation bug, detect translation bug

src/server.py
@@ -9,7 +9,7 @@ import contextlib
 import os
 import numpy as np
 import torch
-from scipy.spatial.transform import Rotation as R
+
 
 from hamer.models import load_hamer, DEFAULT_CHECKPOINT
 from hamer.datasets.vitdet_dataset import ViTDetDataset
@@ -55,6 +55,37 @@ class UDPStreamReceiver:
         self.running = False
         self.sock.close()
 
+def get_hand_frame(keypoint_3d_array: np.ndarray) -> np.ndarray:
+        """
+        Originates from dex-retargeting repo
+        
+        Compute the 3D coordinate frame (orientation only) from detected 3d key points
+        :param points: keypoints detected with HaMeR. Order: [wrist, index, middle, pinky]
+        :return: the coordinate frame of wrist in MANO convention
+        """
+        assert keypoint_3d_array.shape == (21, 3)
+        points = keypoint_3d_array[[0, 5, 9], :]
+
+        # Compute vector from palm to the first joint of middle finger
+        x_vector = points[0] - points[2]
+
+        # Normal fitting with SVD
+        points = points - np.mean(points, axis=0, keepdims=True)
+        u, s, v = np.linalg.svd(points)
+
+        normal = v[2, :]
+
+        # Gram–Schmidt Orthonormalize
+        x = x_vector - np.sum(x_vector * normal) * normal
+        x = x / np.linalg.norm(x)
+        z = np.cross(x, normal)
+
+        # We assume that the vector from pinky to index is similar the z axis in MANO convention
+        if np.sum(z * (points[1] - points[2])) < 0:
+            normal *= -1
+            z *= -1
+        frame = np.stack([x, normal, z], axis=1)
+        return frame
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -104,15 +135,19 @@ def main():
             with torch.no_grad():
                 out = model(batch)
             
-            # mano params and joints
-            joints3d = np.round(out['pred_keypoints_3d'][0].cpu().numpy(), 4)
-            mano_pose_mats = out['pred_mano_params']['hand_pose'][0].cpu().numpy()
-            mano_pose_aa = np.round([R.from_matrix(m.reshape(3, 3)).as_rotvec() for m in mano_pose_mats], 4)
+            # get joint positions & wrist rotation matrix
+            joints3d_global = out['pred_keypoints_3d'][0].cpu().numpy() # joints in camera frame
+            joints3d_global = joints3d_global - joints3d_global[0]
+            
+            wrist_rot = get_hand_frame(joints3d_global)
+            
+            joints3d = np.round(joints3d_global @ wrist_rot, 4) # # joints in hand frame
+            wrist_rot = np.round(wrist_rot, 4)
             
             # send msg
             msg = json.dumps({
                 'joints': joints3d.tolist(),
-                'mano_params': mano_pose_aa,
+                'hand_rotation': wrist_rot.tolist(),
                 'timestamp': time.time()
             }, default=lambda x: x.tolist() if hasattr(x, 'tolist') else float(x)).encode('utf-8')
             

src/visualization/robot_hand_view.py
@@ -1,19 +1,18 @@
 import os
 import trimesh
 import logging
-import contextlib
 import numpy as np
 import rerun as rr
 from scipy.spatial.transform import Rotation as R
 from yourdfpy import URDF
 
-logging.getLogger("yourdfpy").setLevel(logging.ERROR)
-
-MANO_TRANSFORM = np.array([
-    [ 0, -1,  0],
-    [ 1,  0,  0],
-    [ 0,  0, 1]
+R_hamer2urdf = np.array([   # ROTATION ISSUE DEBUG for rh56dftp URDF
+    [-1, 0, 0],
+    [ 0,-1, 0],
+    [ 0, 0, 1] 
 ])
+
+logging.getLogger("yourdfpy").setLevel(logging.ERROR)
 
 class RobotHandView:
     """Render robot hand in Rerun"""
@@ -66,7 +65,7 @@ class RobotHandView:
             
         joints_array = np.array(joints3d)
         joints_array = np.array(joints3d) - np.array(joints3d)[0]
-        joints_array = joints_array @ MANO_TRANSFORM.T
+        joints_array = joints_array @ R_hamer2urdf.T    # ROTATION ISSUE DEBUG for rh56dftp URDF
         
         optimizer = self.retargeter.optimizer
 
@@ -77,7 +76,7 @@ class RobotHandView:
             origin_idx = optimizer.target_link_human_indices[0, :]
             task_idx = optimizer.target_link_human_indices[1, :]
             ref_value = joints_array[task_idx, :] - joints_array[origin_idx, :]
-
+        
         # compute robot angles and update
         qpos = self.retargeter.retarget(ref_value)
         qpos_dict = dict(zip(optimizer.robot.dof_joint_names, qpos))
@@ -96,6 +95,28 @@ class RobotHandView:
                 )
             except Exception:
                 pass
-    
+            
+        # draw target vectors
+        if hasattr(optimizer, "origin_link_names"):
+            origins = np.array([
+                self.robot.get_transform(name)[:3, 3] 
+                for name in optimizer.origin_link_names
+            ])
+        else:
+            origins = np.zeros_like(ref_value)
+        
+        self.draw_target_vectors(origins, ref_value)
+        
+    def draw_target_vectors(self, origins, vectors):
+        rr.log(
+            f"{self.root_entity}/Target_Vectors",
+            rr.Arrows3D(
+                origins=origins,
+                vectors=vectors,
+                colors=[[255, 255, 0] for _ in range(len(vectors))],
+                radii=0.002
+            )
+        )
+
     def close(self):
         self.fnull.close()